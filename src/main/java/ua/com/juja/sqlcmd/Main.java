3b6f246bfbb92e7b37ae4a984778d5630c3fd14e
lyashenko_dmitriy <solykk@yandex.ru>
2017-02-01 17:09:04 +0200

    реализовал нормальный вывод  таблиц

--- src/main/java/ua/com/juja/sqlcmd/Main.java
+++ src/main/java/ua/com/juja/sqlcmd/Main.java
@@ -29,46 +29,65 @@ public class Main {
             System.out.println(tables.get(i));
         }
         System.out.println();
-
-        ArrayList<String> tables2 = manager.getAllColumnNamesFromTable("COUNTRIES");
-
-        for (int i = 0; i < tables2.size(); i++){
-            System.out.println(tables2.get(i));
-        }
-        System.out.println();
+//
+//        ArrayList<String> tables2 = manager.getAllColumnNamesFromTable("COUNTRIES");
+//
+//        for (int i = 0; i < tables2.size(); i++){
+//            System.out.println(tables2.get(i));
+//        }
+//        System.out.println();
 
         String step = "Вы подключились к базе данных. Поздравляю )";
 
         stepPrint(step);
-
-
-
-        ArrayList<String> tables3 = manager.getDataTypeColumnFromTable("EMPLOYEES", "EMPLOYEE_ID");
-
-        for (int i = 0; i < tables3.size(); i++){
-            System.out.println(tables3.get(i));
-        }
         System.out.println();
+//
+//
+//
+//        ArrayList<String> tables3 = manager.getDataTypeColumnFromTable("EMPLOYEES", "EMPLOYEE_ID");
+//
+//        for (int i = 0; i < tables3.size(); i++){
+//            System.out.println(tables3.get(i));
+//        }
+//        System.out.println();
+//
+//        ArrayList<String[]> tables4 = manager.getDataTypeAllColumnsFromTable("EMPLOYEES");
+//
+//        for (int i = 0; i < tables4.size(); i++){
+//            System.out.println(tables4.get(i)[0] + " " + tables4.get(i)[1]);
+//        }
+//        System.out.println();
+//
+//      //  boolean g = manager.createTable("SERT",tables4, false);
+//
+////        System.out.println(g);
+//
+//        for (int i = 0; i < tables4.size(); i++){
+//            System.out.println(tables4.get(i)[0] + " " + tables4.get(i)[1]);
+//        }
+//        System.out.println();
 
-        ArrayList<String[]> tables4 = manager.getDataTypeAllColumnsFromTable("EMPLOYEES");
-
-        for (int i = 0; i < tables4.size(); i++){
-            System.out.println(tables4.get(i)[0] + " " + tables4.get(i)[1]);
-        }
+        printTable(manager.readTable("REGIONS"));
         System.out.println();
-
-      //  boolean g = manager.createTable("SERT",tables4, false);
-
-//        System.out.println(g);
-
-        for (int i = 0; i < tables4.size(); i++){
-            System.out.println(tables4.get(i)[0] + " " + tables4.get(i)[1]);
-        }
+        printTable(manager.readTable("LOCATIONS"));
         System.out.println();
-
+        printTable(manager.readTable("DEPARTMENTS"));
+        System.out.println();
+        printTable(manager.readTable("JOBS"));
+        System.out.println();
         printTable(manager.readTable("EMPLOYEES"));
-
         System.out.println();
+        printTable(manager.readTable("JOB_HISTORY"));
+        System.out.println();
+        printTable(manager.readTable("SERTEE"));
+        System.out.println();
+        printTable(manager.readTable("MYTABLE"));
+        System.out.println();
+        printTable(manager.readTable("SERTE"));
+        System.out.println();
+        printTable(manager.readTable("COUNTRIES"));
+
+
 
         String [] first = new String[]{"SALARY", "17000"};
         String [] second = new String[]{"DEPARTMENT_ID", "90"};
@@ -107,7 +126,7 @@ public class Main {
 
 
         for (int i = 0; i < History.cache.size(); i++){
-            stepPrint(History.cache.get(i));
+            System.out.println(History.cache.get(i));
             System.out.println();
         }
         System.out.println();
@@ -118,23 +137,157 @@ public class Main {
 
     private static void printTable(Table table){
 
-        System.out.println(table.getTableDate().get(0).getValue().size());
-
-        for (int i = 0; i < table.getTableDate().size(); i++){
-            System.out.print(table.getTableDate().get(i).columnName() + "  ");
-
+        if(table == null){
+            System.out.println("BREAK");
+            return;
         }
 
+
+        char simbol = '\u254F';
+        ArrayList<Integer> maxLengs = new ArrayList<>();
+        int sumOfLenght = 0;
+
+
+        for (int j = 0; j < table.getTableDate().size(); j++) {
+
+            String obj;
+            if(table.getTableDate().get(j).getValue().size() == 0){
+                obj = "";
+            } else {
+                obj = table.getTableDate().get(j).getValue().get(0);
+                if (obj == null){
+                    obj = "null";
+                }
+            }
+
+
+
+            Integer tmp = obj.length();
+            Integer tmpCol = table.getTableDate().get(j).columnName().length();
+
+            for (int i = 0; i < table.getTableDate().get(j).getValue().size(); i++) {
+
+                String tempObj = table.getTableDate().get(j).getValue().get(i);
+                if (tempObj == null){
+                    tempObj = "null";
+                }
+
+                Integer tempIns = tempObj.length();
+                if (tmp < tempIns) {
+                    tmp = tempIns;
+                }
+            }
+
+            if(tmpCol > tmp){
+                tmp = tmpCol;
+            }
+
+            maxLengs.add(tmp);
+
+            sumOfLenght += tmp + 3;
+        }
+
+
+        char proba = '\u250C';
+        char [] line = new char[sumOfLenght + 1];
+        for (int q = 0; q < line.length; q++) {
+                line[q] = '-';
+        }
+
+        char [] reBildName = new char[sumOfLenght];
+        char [] strReBildName = table.getTableName().toCharArray();
+
+        for (int j = 0; j < reBildName.length - 1; j++) {
+                reBildName [j] = ' ';
+            }
+
+        int fromToName = (reBildName.length - strReBildName.length)/2;
+
+        for (int j = fromToName, m = 0; m < strReBildName.length; j++, m++) {
+                reBildName[j] = strReBildName[m];
+            }
+
+        System.out.println(new String(line));
+        System.out.print('|' + new String(reBildName) + '|');
         System.out.println();
+        System.out.println(new String(line));
+
+        sleeper(5);
+
+        for (int i = 0; i < table.getTableDate().size(); i++){
+
+            String bil = table.getTableDate().get(i).columnName();
+            char [] reBild = new char[maxLengs.get(i) + 2];
+            char [] strReBild = bil.toCharArray();
+
+            for (int j = 0; j < reBild.length; j++) {
+                reBild [j] = ' ';
+            }
+
+            int fromTo = (reBild.length - strReBild.length)/2;
+
+            for (int j = fromTo, m = 0; m < strReBild.length; j++, m++) {
+                reBild[j] = strReBild[m];
+            }
+
+            if(i == table.getTableDate().size() - 1){
+                System.out.print('|' + new String(reBild) + '|');
+            } else {
+                System.out.print('|' + new String(reBild));
+            }
+
+
+            sleeper(5);
+
+
+        }
+        System.out.println();
+        System.out.println(new String(line));
+
         for (int i = 0; i < table.getTableDate().get(0).getValue().size(); i++){
+
              for (int j = 0; j < table.getTableDate().size(); j++) {
-                 System.out.print(table.getTableDate().get(j).getValue().get(i) + " ");
+
+                 String bil = table.getTableDate().get(j).getValue().get(i);
+
+                 if(bil == null){
+                     bil = "null";
+                 }
+
+                 char [] reBild = new char[maxLengs.get(j) + 2];
+                 char [] strReBild = bil.toCharArray();
+
+                 for (int w = 0; w < reBild.length; w++) {
+                     reBild [w] = ' ';
+                 }
+
+                 int fromTo = (reBild.length - strReBild.length)/2;
+
+                 for (int f = fromTo, m = 0; m < strReBild.length; f++, m++) {
+                     reBild[f] = strReBild[m];
+                 }
+
+                 if(j == table.getTableDate().size() - 1){
+                     System.out.print('|' + new String(reBild) + '|');
+                 } else{
+                     System.out.print('|' + new String(reBild));
+                 }
+
+                 sleeper(5);
+
              }
             System.out.println();
         }
 
+        System.out.println(new String(line));
+    }
 
-
+    private static void sleeper(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void stepPrint(String step) {
@@ -142,15 +295,8 @@ public class Main {
         char [] rezz = step.toCharArray();
 
         for (int i = 0; i < rezz.length; i++){
-
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
+            sleeper(100);
             System.out.print(rezz[i]);
-
         }
     }
 }
